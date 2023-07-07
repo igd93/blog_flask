a27f45a2668117500c68f8aaa155c94c539961be
clockiz <clockiz@example.com>
2023-07-07 11:36:35 +0200

    Created Hello World app, added connection to a mysql database

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
 app = Flask(__name__)
+
+
+username = 'root'
+password = 'Wedge8Quantum@'
+host = 'localhost'
+database = 'blog'
+encoded_password = quote_plus(password)
+url = f"mysql://{username}:{encoded_password}@{host}/{database}"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def hello():
