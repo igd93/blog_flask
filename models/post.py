1984db54d65d6e115383cb0e64d472d87388257e
clockiz <clockiz@example.com>
2023-07-07 21:02:00 +0200

    Created update and delete mappings

--- models/post.py
+++ models/post.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.Text)
+   
 
     def __repr__(self):
         return f"<Post id = {self.id}>"
