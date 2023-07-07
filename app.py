c2a51168bd087ba630e8775c174c38493b02501c
clockiz <clockiz@example.com>
2023-07-07 16:40:45 +0200

    Works with POST method, all is recorded into db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
@@ -18,11 +18,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
+class Post(db.Model):
 
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f<"Post id = {self.id}>"
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    db.session.commit()
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route("/posts", methods=['GET', 'POST'])
+def create_post():
+    content = request.get_json()
+    post = Post(content=content["content"])
+    db.session.add(post)
+    db.session.commit()
+    return 'Post created succesfully'
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
